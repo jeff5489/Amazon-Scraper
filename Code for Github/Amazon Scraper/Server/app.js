b88298e0e508baee06128bb7a5c011f2f3ad5f41
Jeff <jeff5489@gmail.com>
2020-08-13 21:40:31 -0600

    fixed port in server app.js

--- Code for Github/Amazon Scraper/Server/app.js
+++ Code for Github/Amazon Scraper/Server/app.js
@@ -6,8 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
 
 //test change

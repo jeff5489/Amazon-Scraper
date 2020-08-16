47105dd82d47119c7f4cbebbe78de246e402734e
Jeff <jeff5489@gmail.com>
2020-08-15 22:35:52 -0500

    added cors

--- Code for Github/Amazon Scraper/Server/app.js
+++ Code for Github/Amazon Scraper/Server/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const PORT = process.env.PORT || 5000
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
